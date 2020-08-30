8865eb116dfc81cafbc128c71000797dd1f0ec17
Darren Anderson <dranders@colorado.edu>
2020-08-29 17:39:56 -0700

    fetch and log data from model

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-
 import React, {
   useState,
   useEffect,
@@ -7,12 +5,35 @@ import React, {
   FunctionComponent,
 } from 'react';
 
+import Model from '../model';
+
+import IDatum from '../model/interfaces/datum-interface';
+
+const REGION_ID = 'BR-TO';
+
+const SERIES_ID = 'annual-deforestation-rate';
+
 interface IApp {
   apiBaseUri: string;
 }
 
 const App:FunctionComponent<IApp> = (props: IApp) => {
   const { apiBaseUri } = props;
+
+  const [series, setSeries] = useState<IDatum>();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const model = new Model();
+    model.fetchData(apiBaseUri).then(() => {
+      setSeries(model.find({ fieldRef: SERIES_ID, regionRef: REGION_ID }));
+      if (!isReady) setIsReady(true);
+    });
+  }, []);
+
+  if (!isReady) return (<div>... fetching data</div>);
+
+  console.log(series);
 
   return (
     <div>
